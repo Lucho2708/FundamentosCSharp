53308b2cc675100e96e202f10e0f827efba84a6b
Luis Eduardo Silva Rodríguez <lsilva@toyotacredito.com.co>
2023-04-23 12:58:18 -0500

    02. Clases, objetos y constructores

--- FundamentosCSHARP/Program.cs
+++ FundamentosCSHARP/Program.cs
@@ -1,79 +1,14 @@
-﻿//Tipo de dato - "byte"
-
-byte numeroByteMin = 0;
-byte numeroByteMax = 255;
-
-Console.WriteLine($"El tipo \"byte\" contiente 1 Byte qu va desde {numeroByteMin} hasta {numeroByteMax}");
-
-//Tipo de dato - "sbyte"
-
-sbyte numeroSbyteMin = -128;
-sbyte numeroSbyteMax = 127;
-
-Console.WriteLine($"El tipo \"sbyte\" permite tener numeros negativos desde {numeroSbyteMin} hasta {numeroSbyteMax} ");
-
-//Tipo de dato - short
-
-short numeroShortMin = -32768;
-short numeroShortMax = 32767;
-
-Console.WriteLine($"El tipo \"short\" permite tener numeros negativos y positivos desde {numeroShortMin} hasta {numeroShortMax}");
-
-//Tipo de dato - ushort
-
-ushort numeroUshortMin = 0;
-ushort numeroUshortMax = 65535;
-
-Console.WriteLine($"El tipo \"ushort\" permite tener numeros positivos desde {numeroUshortMin} hasta {numeroUshortMax}");
-
-//Tipo de dato - "int"
-
-int numeroIntMin = -2147483648;
-int numeroIntMax = 2147483647;
-
-Console.WriteLine($"El tipo \"int\" permite tener numeros negativos y positivos desde {numeroIntMin} hasta {numeroIntMax}");
-
-//Tipo de dato - uint
-
-uint numeroUintMin = 0;
-uint numeroUintMax = 4294967295;
-
-Console.WriteLine($"El tipo \"uint\" permite tener numeros positivos desde {numeroUintMin} hasta {numeroUintMax}");
-
-//Tipo de dato - "long"
-
-long numeroLongMin = -9223372036854775808;
-long numeroLongMax = 9223372036854775807;
-
-Console.WriteLine($"El tipo \"long\" permite tener numeros positivos y negativos desde {numeroLongMin} hasta {numeroLongMax}");
-
-//Tipo de dato - "ulong"
-
-ulong numeroUlongMin = 0;
-ulong numeroUlongMax = 18446744073709551615;
-
-Console.WriteLine($"El tipo \"ulong\" permite tener numeros positivos desde {numeroUlongMin} hasta {numeroUlongMax}");
-
-//Tipo de datos - "float - double - decimal"
-
-float numeroFloat =  10.2f;
-double numeroDouble = 10.2d;
-decimal numeroDecimal = 10.2m;
-
-Console.WriteLine($"El tipo \"float - double - decimal \" float {numeroFloat}f, double {numeroDouble}d y decimal {numeroDecimal}m");
-
-//Tipo de dato simple - "char" se represeta con ' ' comillas simple
-
-char caracter = 'A';
-
-//Tipo de dato string compuesto - "string" se representa con " " comillas dobles 
-
-string palabra = "Palabra";
-
-//Tipo "DateTime" fecha
-
-DateTime date = DateTime.Now;
-
-//Tipo de dato "bool" SI o NO
-
-bool siOno = false;
+﻿using System;
+using FundamentosCSHARP.Models;
+namespace FundametosCSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cerveza cerveza = new Cerveza(1000);
+            cerveza.Beberse(200);
+            Console.WriteLine(cerveza.Cantidad);
+        }
+    }
+}
