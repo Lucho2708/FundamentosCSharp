93fa41499316f6df4b8fcd9eea548c841bdaeda1
Luis Eduardo Silva Rodríguez <lsilva@toyotacredito.com.co>
2023-04-25 08:33:54 -0500

    03. Arreglos y listas

--- FundamentosCSHARP/Program.cs
+++ FundamentosCSHARP/Program.cs
@@ -6,9 +6,37 @@ namespace FundametosCSHARP
     {
         static void Main(string[] args)
         {
-            Cerveza cerveza = new Cerveza(1000);
-            cerveza.Beberse(200);
-            Console.WriteLine(cerveza.Cantidad);
+            int[] numeros = new int[10] {1,2,3,4,5,6,7,8,9,10};
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+
+            foreach (var item in numeros)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("****************************************");
+            List<int> lista = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8};
+            lista.Add(9);
+            lista.Add(10);
+            lista.Remove(2);
+
+            foreach (var item in lista)
+            {
+                Console.WriteLine("Elemento: "+item);
+            }
+
+            List<Cerveza> cervezas = new List<Cerveza>() { new Cerveza(10, "Cerveza premiun") };
+            cervezas.Add(new Cerveza(500, "Aguila"));
+            Cerveza erdinger = new Cerveza(500, "Cerveza de trigo");
+            cervezas.Add(erdinger);
+
+            foreach (var item in cervezas)
+            {
+                Console.WriteLine("Cerveza: " + item.Nombre);
+            }
         }
     }
 }
